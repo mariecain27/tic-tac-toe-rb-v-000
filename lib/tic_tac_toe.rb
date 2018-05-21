3ad7e2a32d5b3a351a24458bd53fa081881318da
Marie Cain <mariecain27@gmail.com>
2018-05-21 03:10:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -103,9 +103,9 @@ end
 
 def current_player(board) 
   if turn_count(board).even?
-    player = "X"
-  elsif turn_count(board).odd?
     player = "O"
+  elsif turn_count(board).odd?
+    player = "X"
   end
   return player
 end
