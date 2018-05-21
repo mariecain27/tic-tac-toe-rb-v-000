3310e05f657af9521afd6906909350ec6b717046
Marie Cain <mariecain27@gmail.com>
2018-05-21 02:02:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,9 +11,9 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-#def move(board, index, current_player = "X")
-#  board[index] = current_player
-#end
+def move(board, index, current_player)
+  board[index] = current_player
+end
 
 #def position_taken?(board, location)
 #  board[location] != " " && board[location] != ""
