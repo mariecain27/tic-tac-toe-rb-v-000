1c945bebb4b515be834521f4194d739c4fb8241a
Marie Cain <mariecain27@gmail.com>
2018-05-21 01:58:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,14 +41,33 @@ WIN_COMBINATIONS = [
 # end 
 #end
 
-def over?(board)
-  won?(board) || draw?(board) || full?(board) 
-end
+#def over?(board)
+#  won?(board) || draw?(board) || full?(board) 
+#end
 
-def winner(board)
- if won?(board)
-   board[won?(board).first]
- else
-   nil
- end
-end
+#def winner(board)
+# if won?(board)
+#   board[won?(board).first]
+# else
+#   nil
+# end
+#end
+
+#def turn_count(board)
+#  counter = 0
+#  board.each do |element|
+#    if element == "X" || element == "O"
+#     counter += 1
+#    end 
+#  end
+#  return counter
+#end
+
+#def current_player(board)
+#  if turn_count(board).even?
+#    player = "X"
+#  elsif turn_count(board).odd?
+#    player = "O"
+#  end
+#  return player
+#end
