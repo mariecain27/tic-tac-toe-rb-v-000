ac496b3298f16a3b1c1aa47a6e6a1249569fd8d6
Marie Cain <mariecain27@gmail.com>
2018-05-21 02:07:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,15 +101,15 @@ def winner(board)
  end
 end
 
-#def turn_count(board)
-#  counter = 0
-#  board.each do |element|
-#    if element == "X" || element == "O"
-#     counter += 1
-#    end 
-#  end
-#  return counter
-#end
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+     counter += 1
+    end 
+  end
+  return counter
+end
 
 #def current_player(board)
 #  if turn_count(board).even?
